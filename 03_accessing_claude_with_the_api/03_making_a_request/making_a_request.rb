ac105693e7c37ed4443c232efdd728135d3c3096
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-07-20 13:10:17 -0400

    Add making a request example

---
+++ 03_accessing_claude_with_the_api/03_making_a_request/making_a_request.rb
@@ -0,0 +1,19 @@
+require "bundler/setup"
+require "dotenv/load"
+require "anthropic"
+
+client = Anthropic::Client.new
+model = "claude-sonnet-4-0"
+
+message = client.messages.create(
+  model: model,
+  max_tokens: 1000,
+  messages: [
+    {
+      role: "user",
+      content: "What is quantum computing? Answer in one sentence"
+    }
+  ]
+)
+
+puts message.content[0].text
