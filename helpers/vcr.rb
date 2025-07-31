e7a976b3fa51cc94ac08a4c0b41f5abae68d7ece
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-07-31 08:48:29 -0400

    Fix VCR

--- helpers/vcr.rb
+++ helpers/vcr.rb
@@ -6,9 +6,3 @@ VCR.configure do |config|
   config.filter_sensitive_data("<X-API-KEY>") { |interaction| interaction.request.headers["X-Api-Key"]&.first }
   config.filter_sensitive_data("<AUTHORIZATION>") { |interaction| interaction.request.headers["Authorization"]&.first }
 end
-
-def VCR.use_cassette(cassette_name, &block)
-  VCR.use_cassette(cassette_name) do
-    yield
-  end
-end
